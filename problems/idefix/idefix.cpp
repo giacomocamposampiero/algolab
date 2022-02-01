07287827c5e08064242dfb89312c2cbade39a26a
Giacomo Camposampiero <19869776+giacomocamposampiero@users.noreply.github.com>
2022-02-01 18:43:04 +0100

    Update idefix.cpp

--- problems/idefix/idefix.cpp
+++ problems/idefix/idefix.cpp
@@ -1,3 +1,4 @@
+///1
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
@@ -32,17 +33,8 @@ void testcase() {
     int x, y; std::cin >> x >> y;
     pts.emplace_back(K::Point_2(x, y), i);
   }
-
   Delaunay t;
   t.insert(pts.begin(), pts.end());
-  
-  std::vector<std::vector<long>> oak_bones(n);
-  for(int i=0; i<m; i++) {
-    int x, y; std::cin >> x >> y;
-    K::Point_2 bone(x,y);
-    auto nv = t.nearest_vertex(bone);
-    oak_bones[nv -> info()].push_back(CGAL::squared_distance(bone, nv -> point()));
-  }
 
   EdgeV edges;
   for (auto e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e) {
@@ -56,39 +48,60 @@ void testcase() {
     return std::get<2>(e1) < std::get<2>(e2);
   });
 
-  
   boost::disjoint_sets_with_storage<> uf(n);
   Index n_components = n;
-  int a = 0, max_b = 0;
-  std::vector<int> tot(n, 0);
   
-  for(int i=0; i<n; i++) {
-    std::sort(oak_bones[i].begin(), oak_bones[i].end());
-    auto it = std::upper_bound(oak_bones[i].begin(), oak_bones[i].end(), s/4);
-    tot[i] = std::distance(oak_bones[i].begin(), it);
-    max_b = std::max(max_b, tot[i]);
-  }
-  
-  for (EdgeV::const_iterator e = edges.begin(); e != edges.end() && a ==0; ++e) {
+  for (EdgeV::const_iterator e = edges.begin(); e != edges.end(); ++e) {
+    if(std::get<2>(*e) > s) break;
     Index c1 = uf.find_set(std::get<0>(*e));
     Index c2 = uf.find_set(std::get<1>(*e));
-    
-    if(a == 0 && std::get<2>(*e) > s) a = max_b;
-    
     if (c1 != c2) {
-      int new_tot = tot[c1] + tot[c2];
-      if(new_tot > max_b) max_b = new_tot;
-      
       uf.link(c1, c2);
-      
-      int new_class = uf.find_set(std::get<0>(*e));
-      tot[new_class] = new_tot;
       --n_components;
     }
     if(n_components == 1) break;
   }
   
-  std::cout << a << ' ' << 4*s << std::endl;
+  std::vector<int> bones(n, 0);
+  std::vector<int> cbones(n+m, 0);
+  for(int i=0; i<m; i++) {
+    
+    int x, y; std::cin >> x >> y;
+    K::Point_2 bone(x,y);
+    
+    auto nv = t.nearest_vertex(bone);
+    K::FT distance = 4*CGAL::squared_distance(bone, nv -> point());
+    if(distance <= s) bones[uf.find_set(nv -> info())]++;
+    
+    edges.emplace_back(nv -> info(), n+i, distance);
+    cbones[n+i]++;
+  }
+
+  int a = 0;
+  for(int i=0; i<n; i++) a = std::max(a, bones[i]);
+  
+  std::sort(edges.begin(), edges.end(), [](const Edge& e1, const Edge& e2) -> bool {
+    return std::get<2>(e1) < std::get<2>(e2);
+  });
+  
+  boost::disjoint_sets_with_storage<> uf2(n+m);
+  n_components = n+m;
+  
+  long b = 4*s;
+  for (EdgeV::const_iterator e = edges.begin(); e != edges.end(); ++e) {
+    Index c1 = uf2.find_set(std::get<0>(*e));
+    Index c2 = uf2.find_set(std::get<1>(*e));
+    if (c1 != c2) {
+      uf2.link(c1, c2);
+      int comp = uf2.find_set(std::get<0>(*e));
+      cbones[comp] = cbones[c2] + cbones[c1];
+      if(cbones[comp] >= k) { b=std::get<2>(*e); break; }
+      --n_components;
+    }
+    if(n_components == 1) break;
+  }
+  
+  std::cout << a << ' ' << b << std::endl;
 }
 
 int main() {
